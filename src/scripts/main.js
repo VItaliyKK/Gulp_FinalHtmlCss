1a47c9a4982160a423680e236b8ad6870b7c6d38
Vitaliy Karpinets <vitalik.karpinets@gmail.com>
2021-02-18 21:00:57 +0200

    scss loops added / stylization and html structure changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
-console.warn('••••• Helloooy, first GULP project!))))) •••••');
+$(document).ready(styleHeader)
+
+$(window).on("resize", styleHeader)
+
+$(window).on("scroll", styleHeader)
+
+// ** gradual background display ** 
+function styleHeader(){
+	$('.header_wrapper').css({
+        backgroundColor: `rgba(255 78 80 / ${
+        	window.scrollY <= 360 ? window.scrollY/4 : 90
+        }%)`
+    })
+}; 
